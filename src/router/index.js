9730ec9d4ef168e89a0f95f4bacd3a477e65ee69
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-08-22 15:54:59 +0200

    added basic app structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,45 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+
+import AddProduct from "../views/AddProduct.vue";
 import Home from "../views/Home.vue";
+import ProductList from "../views/ProductList.vue";
+import SingleProduct from "../views/SingleProduct.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+	},
+	{
+		path: "/about",
+		name: "About",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/About.vue"),
+	},
+	{
+		path: "/products",
+		name: "products",
+		component: ProductList,
+	},
+	{
+		path: "/product/:slug",
+		name: "product",
+		component: SingleProduct,
+	},
+	{
+		path: "/add-product",
+		name: "add-product",
+		component: AddProduct,
+	},
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+	history: createWebHashHistory(),
+	routes,
 });
 
 export default router;
---
+++ src/views/ProductList.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="">
+		<h1>Product List</h1>
+	</div>
+</template>
